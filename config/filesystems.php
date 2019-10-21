aa5f218fb26d585b5b2c47c5002b873fc807b35b
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-10-22 04:59:22 +0500

    Setup the dropbox api in DropboxServiceProvider

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'dropbox' => [
+            'driver' => 'dropbox',
+            'authorization_token' => env('DROPBOX_TOKEN')
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
