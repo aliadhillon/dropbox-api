9cb33a90dafc246afda0a5027d82d317bbd38123
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-10-22 05:44:40 +0500

    Creates a profile page with form to upload avatar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a href="{{ route('profile') }}" class="dropdown-item">
+                                        {{ __('User Profile') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
