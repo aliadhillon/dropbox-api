08e2c4d147ae8cd4ab874790292fb82958d512a8
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-10-22 05:15:48 +0500

    Adds avatar in migration and form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
         <!-- Styles -->
         <style>
             html, body {
